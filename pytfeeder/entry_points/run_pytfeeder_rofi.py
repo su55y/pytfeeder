2db250127244dfe215863677d4d1dfafa3c9dbc6
su55y <cesarmilano88@yahoo.com>
2023-06-20 18:52:58 +0300

    printer class

--- pytfeeder/entry_points/run_pytfeeder_rofi.py
+++ pytfeeder/entry_points/run_pytfeeder_rofi.py
@@ -1,13 +1,12 @@
 import argparse
 import asyncio
 import logging
-import sys
-from typing import Any, Dict, List
+from typing import List, Optional
 
 from pytfeeder.config import Config
 import pytfeeder.dirs as dirs
 from pytfeeder.feeder import Feeder
-from pytfeeder.models import Entry, Channel
+from pytfeeder.models import Entry
 from pytfeeder.storage import Storage
 
 
@@ -23,6 +22,7 @@ def init_logger(**kwargs):
 
 
 def parse_args() -> argparse.Namespace:
+    DEFAULT_FMT = "{title}\000info\037{id}"
     parser = argparse.ArgumentParser(description="package: %s" % __package__)
     parser.add_argument(
         "-c",
@@ -31,30 +31,77 @@ def parse_args() -> argparse.Namespace:
         help="Location of config file (default: %(default)s)",
     )
     parser.add_argument(
-        "-d",
-        "--cache-dir",
-        help="Location of cache directory (default: %s)" % dirs.default_cachedir_path(),
+        "--channels-fmt",
+        default=DEFAULT_FMT,
+        metavar="STR",
+        help="Channels print format (default: '%s')" % r"{title}\000info\037{id}",
     )
-    parser.add_argument("-i", "--channel-id", help="print channel feed")
-    parser.add_argument("-s", "--sync", action="store_true", help="just update feeds")
-    parser.add_argument("-f", "--feed", action="store_true", help="common feed")
+    parser.add_argument(
+        "--entries-fmt",
+        default=DEFAULT_FMT,
+        metavar="STR",
+        help="Entries print format (default: '%s')" % r"{title}\000info\037{id}",
+    )
+    parser.add_argument(
+        "-i",
+        "--channel-id",
+        metavar="ID",
+        help="Prints channel feed by given channel_id",
+    )
+    parser.add_argument("-f", "--feed", action="store_true", help="Prints common feed")
+    parser.add_argument(
+        "-l", "--limit", type=int, metavar="INT", help="Use custom lines limit"
+    )
+    parser.add_argument(
+        "-s", "--sync", action="store_true", help="Just update feeds and exit"
+    )
 
     return parser.parse_args()
 
 
-def print_entries(entries: List[Entry]):
-    highlight = []
-    for i, e in enumerate(entries):
-        if not e.is_viewed:
-            highlight.append(str(i + 1))
-        print("%s\000info\037%s" % (e.title, e.id))
+class RofiPrinter:
+    def __init__(
+        self,
+        feeder: Feeder,
+        config: Config,
+        channels_fmt: str,
+        entries_fmt: str,
+        limit: Optional[int] = None,
+    ) -> None:
+        self.config = config
+        self.feeder = feeder
+        self.channels_fmt = channels_fmt
+        self.entries_fmt = entries_fmt
+        self.limit = limit
 
-    print("\000active\037%s" % ",".join(highlight))
+    def print_channels(self) -> None:
+        for channel in self.feeder.channels:
+            print(self.channels_fmt.format(title=channel.title, id=channel.channel_id))
 
+    def print_common_feed(self) -> None:
+        self._print_entries(
+            self.feeder.common_feed(self.limit or self.config.common_feed_limit)
+        )
 
-def print_channels(channels: List[Channel]):
-    for c in channels:
-        sys.stdout.write("%s\000info\037%s\n" % (c.title, c.channel_id))
+    def print_channel_feed(self, channel_id: str) -> None:
+        if len(channel_id) != 24:
+            exit("invalid channel_id")
+        self._print_entries(
+            self.feeder.channel_feed(
+                channel_id, self.limit or self.config.channel_feed_limit
+            )
+        )
+
+    def _print_entries(self, entries: List[Entry]):
+        if not entries:
+            print("\000message\037no entries")
+        highlight = []
+        for i, entry in enumerate(entries):
+            if not entry.is_viewed:
+                highlight.append(str(i + 1))
+            print(self.entries_fmt.format(title=entry.title, id=entry.id))
+
+        print("\000active\037%s" % ",".join(highlight))
 
 
 def run():
@@ -63,16 +110,14 @@ def run():
     if not config:
         exit(1)
 
-    cache_dir = args.cache_dir or dirs.default_cachedir_path()
+    cache_dir = dirs.default_cachedir_path()
     if not cache_dir.exists():
         cache_dir.mkdir(parents=True)
 
-    logger_opts: Dict[str, Any] = {
-        "file": config.log_file or dirs.default_logfile_path()
-    }
-    if config.log_level is not None:
-        logger_opts["level"] = config.log_level
-    init_logger(**logger_opts)
+    init_logger(
+        file=config.log_file or dirs.default_logfile_path(),
+        level=config.log_level,
+    )
 
     db_file = config.storage_path or dirs.default_storage_path()
     feeder = Feeder(config, Storage(db_file))
@@ -80,9 +125,16 @@ def run():
         feeder.sync_channels()
         asyncio.run(feeder.sync_entries())
         exit(0)
+    printer = RofiPrinter(
+        feeder=feeder,
+        config=config,
+        channels_fmt=args.channels_fmt,
+        entries_fmt=args.entries_fmt,
+        limit=args.limit,
+    )
     if args.channel_id:
-        print_entries(feeder.channel_feed(args.channel_id))
+        printer.print_channel_feed(args.channel_id)
     elif args.feed:
-        print_entries(feeder.common_feed())
+        printer.print_common_feed()
     else:
-        print_channels(feeder.channels)
+        printer.print_channels()
