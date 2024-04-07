259da02428d0068508f9b43c0ffe27084a5abc1b
su55y <cesarmilano88@yahoo.com>
2024-04-08 00:23:26 +0300

    updated rofi example

--- examples/rofi/helper.sh
+++ examples/rofi/helper.sh
@@ -19,8 +19,23 @@ err_msg() {
 start_menu() {
 	printf "\000markup-rows\037true\n"
 	printf "feed\r%s new entries\000info\037feed\n" "$(pytfeeder -u)"
-	pytfeeder --rofi --channels-fmt '{title}\r{unviewed_count} new entries\000info\037{id}'
+	pytfeeder --rofi --channels-fmt '{title}\r{unviewed_count} new entries\000info\037{id}' "$@"
 	printf "\000new-selection\0370\n"
+}
+
+print_feed() {
+	printf "back\000info\037main\n"
+	printf "\000markup-rows\037true\n"
+	pytfeeder --rofi -f --entries-fmt '{title}\r<b><i>{channel_title}</i></b>\000info\037{id}\037meta\037{meta}' "$@"
+	printf "\000new-selection\0370\n"
+}
+
+print_channel_feed() {
+	[ -n "$1" ] || return 1
+	channel_id="$1"
+	shift
+	printf "back\000info\037main\n"
+	pytfeeder --rofi -i="$channel_id" "$@" --entries-fmt '{title}\r<b>{updated}</b>\000info\037{id}\037meta\037{meta}' --datetime-fmt '<i>%d %B</i>'
 }
 
 printf "\000use-hot-keys\037true\n"
@@ -31,18 +46,12 @@ case $ROFI_RETV in
 # select line
 1)
 	case "$ROFI_INFO" in
-	feed)
-		printf "back\000info\037main\n"
-		printf "\000markup-rows\037true\n"
-		pytfeeder --rofi -f --entries-fmt '{title}\r<b><i>{channel_title}</i></b>\000info\037{id}\037meta\037{meta}'
-		printf "\000new-selection\0370\n"
-		;;
+	feed) print_feed ;;
 	main) start_menu ;;
 	*)
 		if [ "$(printf '%s' "$ROFI_INFO" |
 			grep -oP "^[0-9a-zA-Z_\-]{24}$")" = "$ROFI_INFO" ]; then
-			printf "back\000info\037main\n"
-			pytfeeder --rofi -i="$ROFI_INFO" --entries-fmt '{title}\r<b>{updated}</b>\000info\037{id}\037meta\037{meta}' --datetime-fmt '<i>%d %B</i>'
+			print_channel_feed "$ROFI_INFO"
 			printf "\000new-selection\0370\n"
 		elif [ "$(printf '%s' "$ROFI_INFO" |
 			grep -oP "^[0-9a-zA-Z_\-]{11}$")" = "$ROFI_INFO" ]; then
@@ -56,16 +65,14 @@ case $ROFI_RETV in
 	;;
 # kb-custom-1 (Ctrl-s) -- sync
 10)
-	[ "$ROFI_DATA" = "main" ] || printf "back\000info\037main\n"
 	case $ROFI_DATA in
-	feed) pytfeeder --rofi -s -f ;;
+	feed) print_feed "-s" ;;
 	main)
-		printf "feed\000info\037feed\n"
-		pytfeeder --rofi -s
+		start_menu "-s"
 		;;
 	*)
 		[ "${#ROFI_DATA}" -eq 24 ] || err_msg "invalid channel_id '$ROFI_DATA'"
-		pytfeeder --rofi -s -i="$ROFI_DATA"
+		print_channel_feed "$ROFI_DATA" "-s"
 		;;
 	esac
 	;;
@@ -74,41 +81,38 @@ case $ROFI_RETV in
 # kb-custom-3 (Ctrl-x) -- mark entry as viewed
 # kb-custom-6 (Ctrl-d) -- download selected entry
 12 | 15)
-	[ "$ROFI_DATA" = "main" ] || printf "back\000info\037main\n"
 	[ "${#ROFI_INFO}" -eq 11 ] || err_msg "invalid id '$ROFI_INFO'"
 	case $ROFI_DATA in
-	feed) pytfeeder --rofi -v="$ROFI_INFO" -f ;;
+	feed) print_feed "-v=$ROFI_INFO" ;;
 	*)
 		[ "${#ROFI_DATA}" -eq 24 ] || err_msg "invalid channel_id '$ROFI_DATA'"
-		pytfeeder --rofi -v="$ROFI_INFO" -i="$ROFI_DATA"
+		print_channel_feed "$ROFI_DATA" "-v=$ROFI_INFO"
 		;;
 	esac
 	[ "$ROFI_RETV" -eq 15 ] && download_vid "$ROFI_INFO" "$1"
 	;;
 # kb-custom-4 (Ctrl-X) -- mark current feed entries as viewed
 13)
-	[ "$ROFI_DATA" = "main" ] || printf "back\000info\037main\n"
 	case $ROFI_DATA in
-	feed) pytfeeder --rofi -v all -f ;;
-	main) pytfeeder --rofi -v all ;;
+	feed) print_feed "-v" "all" ;;
+	main) start_menu "-v" "all" ;;
 	*)
 		[ "${#ROFI_DATA}" -eq 24 ] || err_msg "invalid channel_id '$ROFI_DATA'"
-		pytfeeder --rofi -v="$ROFI_DATA" -i="$ROFI_DATA"
+		print_channel_feed "$ROFI_DATA" "-v=$ROFI_DATA"
 		;;
 	esac
 	printf "\000new-selection\0370"
 	;;
 # kb-custom-5 (Ctrl-a) -- append selected to playlist
 14)
-	[ "$ROFI_DATA" = "main" ] || printf "back\000info\037main\n"
 	[ -f "$APPEND_SCRIPT" ] || err_msg "append script not found"
 	[ "${#ROFI_INFO}" -eq 11 ] || err_msg "invalid id '$ROFI_INFO'"
 	setsid -f "$APPEND_SCRIPT" "https://youtu.be/$ROFI_INFO" >/dev/null 2>&1
 	case $ROFI_DATA in
-	feed) pytfeeder --rofi -v="$ROFI_INFO" -f ;;
+	feed) print_feed "-v=$ROFI_INFO" ;;
 	*)
 		[ "${#ROFI_DATA}" -eq 24 ] || err_msg "invalid channel_id '$ROFI_DATA'"
-		pytfeeder --rofi -v="$ROFI_INFO" -i="$ROFI_DATA"
+		print_channel_feed "$ROFI_DATA" "-v=$ROFI_INFO"
 		;;
 	esac
 	;;
