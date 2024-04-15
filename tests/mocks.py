dae8b7460dfb68341ccf8f43111c3b2f933dc189
su55y <cesarmilano88@yahoo.com>
2024-04-16 00:05:01 +0300

    tests fix

--- tests/mocks.py
+++ tests/mocks.py
@@ -5,30 +5,27 @@ from pytfeeder.models import Entry, Channel
 sample_channel = Channel(channel_id="sample_channel_id1234567", title="Sample Channel")
 sample_entries = [
     Entry(
-        id=f"video_id_{n:02d}",
-        title=f"Video #{n}",
-        updated=str(datetime.now(timezone.utc) - timedelta(hours=25 * (3 - n))),
+        id=f"video_id_{i:02d}",
+        title=f"Video #{i}",
+        updated=datetime.now(timezone.utc) - timedelta(hours=25 * (3 - i)),
         channel_id="sample_channel_id1234567",
     )
-    for n in range(3, 0, -1)
+    for i in range(3, 0, -1)
 ]
 
-entry_fmt = """
-\t<entry>
+entry_fmt = """\t<entry>
 \t\t<yt:videoId>{id}</yt:videoId>
 \t\t<yt:channelId>{channel_id}</yt:channelId>
 \t\t<title>{title}</title>
 \t\t<updated>{updated}</updated>
-\t</entry>
-"""
-feed_fmt = """
-<?xml version="1.0" encoding="UTF-8"?>
+\t</entry>"""
+
+feed_fmt = """<?xml version="1.0" encoding="UTF-8"?>
 <feed xmlns:yt="http://www.youtube.com/xml/schemas/2015" xmlns:media="http://search.yahoo.com/mrss/" xmlns="http://www.w3.org/2005/Atom">
 \t<title>{channel_title}</title>
 \t<link rel="alternate" href="https://www.youtube.com/channel/{channel_id}"/>
 {entries}
-</feed>
-"""
+</feed>"""
 
 raw_feed = feed_fmt.format(
     channel_title=sample_channel.title,
