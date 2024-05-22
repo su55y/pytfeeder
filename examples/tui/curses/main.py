229fb945d27ac45861b52c2eed9707b9e395fd06
su55y <cesarmilano88@yahoo.com>
2024-05-22 21:32:58 +0300

    curses example: left/right keys swap

--- examples/tui/curses/main.py
+++ examples/tui/curses/main.py
@@ -173,10 +173,10 @@ class Picker:
                 case Key.k | curses.KEY_UP:
                     if len(self.lines) > 0:
                         self.move_up()
-                case Key.l | curses.KEY_LEFT:
+                case Key.l | curses.KEY_RIGHT:
                     if len(self.lines) > 0:
                         self.move_right()
-                case Key.h | curses.KEY_RIGHT:
+                case Key.h | curses.KEY_LEFT:
                     self.move_left()
                 case Key.K:
                     self.move_top()
