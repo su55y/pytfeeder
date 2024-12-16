d49c08ebcc2c5d8cd4dfac994a4efa6bf5facad4
su55y <cesarmilano88@yahoo.com>
2024-12-16 14:44:35 +0200

    pytfeeder: consts upd

--- pytfeeder/consts.py
+++ pytfeeder/consts.py
@@ -2,8 +2,10 @@ DEFAULT_LOG_FMT = (
     "[%(asctime)-.19s %(levelname)s] %(message)s (%(filename)s:%(lineno)d)"
 )
 
-DEFAULT_CHANNEL_FMT = "{title}\000info\037{id}"
-DEFAULT_ENTRY_FMT = "{title}\000info\037{id}\037meta\037{meta}"
+DEFAULT_CHANNELS_FMT = "{title}\000info\037{id}"
+DEFAULT_ENTRIES_FMT = "{title}\000info\037{id}\037meta\037{meta}"
+DEFAULT_ROFI_CHANNELS_FMT = "{title}\000info\037{id}"
+DEFAULT_ROFI_ENTRIES_FMT = "{title}\000info\037{id}\037meta\037{meta}"
 DEFAULT_DATETIME_FMT = "%D %T"
 
 YT_FEED_URL = "https://www.youtube.com/feeds/videos.xml?channel_id=%s"
