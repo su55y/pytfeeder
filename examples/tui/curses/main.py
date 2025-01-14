6fdf4aabadd4dfe771b1b6b90755b83cceb2b9bd
su55y <cesarmilano88@yahoo.com>
2025-01-14 17:33:35 +0200

    curses example: use mascos from tui.config

--- examples/tui/curses/main.py
+++ examples/tui/curses/main.py
@@ -1048,10 +1048,10 @@ if __name__ == "__main__":
         config.feed_entries_fmt or DEFAULT_FEED_ENTRIES_FMT
     )
 
-    kwargs["macro1"] = kwargs.get("macro1") or config.macro1
-    kwargs["macro2"] = kwargs.get("macro2") or config.macro2
-    kwargs["macro3"] = kwargs.get("macro3") or config.macro3
-    kwargs["macro4"] = kwargs.get("macro4") or config.macro4
+    kwargs["macro1"] = kwargs.get("macro1") or config.tui.macro1
+    kwargs["macro2"] = kwargs.get("macro2") or config.tui.macro2
+    kwargs["macro3"] = kwargs.get("macro3") or config.tui.macro3
+    kwargs["macro4"] = kwargs.get("macro4") or config.tui.macro4
     kwargs["update_label"] = update_label
     kwargs["last_update_fmt"] = kwargs.get("last_update_fmt") or DEFAULT_LAST_UPDATE_FMT
 
