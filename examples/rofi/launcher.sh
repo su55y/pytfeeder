8b7534a11205be9bcf56db32c5b7f3dc9e06e731
su55y <cesarmilano88@yahoo.com>
2025-01-17 13:29:05 +0200

    rofi example: fix kb, fmt

--- examples/rofi/launcher.sh
+++ examples/rofi/launcher.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 SCRIPTPATH="$(
-	cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1
-	pwd -P
+    cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1
+    pwd -P
 )"
 
 [ -f "$SCRIPTPATH/helper.sh" ] || {
-	notify-send -i "rofi" -a "youtube feed" "helper script not found"
-	exit 1
+    notify-send -i "rofi" -a "youtube feed" "helper script not found"
+    exit 1
 }
 
 # theme string
 theme() {
-	cat <<EOF
+    cat <<EOF
 configuration {
   font: "NotoSans Nerd Font 16";
 }
@@ -34,14 +34,15 @@ EOF
 }
 
 SCRIPTPATH="$SCRIPTPATH" rofi -i -show "pytfeeder-rofi-launcher" \
-	-modi "pytfeeder-rofi-launcher:$SCRIPTPATH/helper.sh" \
-	-no-config -kb-custom-1 "Ctrl+s" \
-	-kb-custom-2 "Ctrl+c" \
-	-kb-custom-3 "Ctrl+x" \
-	-kb-custom-4 "Ctrl+X" \
-	-kb-move-front "" \
-	-kb-custom-5 "Ctrl+a" \
-	-kb-remove-char-forward "Delete" \
-	-kb-custom-6 "Ctrl+d" \
-	-theme-str "$(theme)" \
-	-normal-window -eh 2
+    -modi "pytfeeder-rofi-launcher:$SCRIPTPATH/helper.sh" \
+    -no-config -kb-custom-1 "Ctrl+s" \
+    -kb-secondary-copy "Ctrl+y" \
+    -kb-custom-2 "Ctrl+c" \
+    -kb-custom-3 "Ctrl+x" \
+    -kb-custom-4 "Ctrl+X" \
+    -kb-move-front "" \
+    -kb-custom-5 "Ctrl+a" \
+    -kb-remove-char-forward "Delete" \
+    -kb-custom-6 "Ctrl+d" \
+    -theme-str "$(theme)" \
+    -normal-window -eh 2
