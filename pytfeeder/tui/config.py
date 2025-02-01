e4cfffef8c6f23c174ba4dced84c8c8523601bf5
su55y <cesarmilano88@yahoo.com>
2025-02-01 17:26:45 +0200

    pytfeeder: new_mark property in tui.config

--- pytfeeder/tui/config.py
+++ pytfeeder/tui/config.py
@@ -5,6 +5,7 @@ DEFAULT_CHANNELS_FMT = "{new_mark} | {title}"
 DEFAULT_ENTRIES_FMT = "{new_mark} | {updated} | {title}"
 DEFAULT_FEED_ENTRIES_FMT = "{new_mark} | {updated} | {channel_title} | {title}"
 DEFAULT_LAST_UPDATE_FMT = "%D %T"
+DEFAULT_NEW_MARK = "[+]"
 DEFAULT_STATUS_FMT = "{msg}{index} {title} {keybinds}"
 
 
@@ -16,6 +17,7 @@ class ConfigTUI:
     feed_entries_fmt: str = DEFAULT_FEED_ENTRIES_FMT
     hide_feed: bool = False
     last_update_fmt: str = DEFAULT_LAST_UPDATE_FMT
+    new_mark: str = DEFAULT_NEW_MARK
     status_fmt: str = DEFAULT_STATUS_FMT
     update_interval: Optional[int] = None
     macro1: str = ""
@@ -36,6 +38,8 @@ class ConfigTUI:
             self.hide_feed = hide_feed
         if last_update_fmt := kw.get("last_update_fmt"):
             self.last_update_fmt = last_update_fmt
+        if new_mark := kw.get("new_mark"):
+            self.new_mark = new_mark
         if status_fmt := kw.get("status_fmt"):
             self.status_fmt = status_fmt
         if update_interval := kw.get("update_interval"):
@@ -57,6 +61,7 @@ class ConfigTUI:
         repr_str += f"  feed_entries_fmt: {self.feed_entries_fmt!r}\n"
         repr_str += f"  hide_feed: {self.hide_feed}\n"
         repr_str += f"  last_update_fmt: {self.last_update_fmt!r}\n"
+        repr_str += f"  new_mark: {self.new_mark!r}\n"
         repr_str += f"  status_fmt: {self.status_fmt!r}\n"
         repr_str += f"  update_interval: {self.update_interval}\n"
         repr_str += f"  macro1: {self.macro1}\n"
