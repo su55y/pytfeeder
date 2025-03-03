d54802407654639f9a609fe6b916c125a233f130
su55y <cesarmilano88@yahoo.com>
2025-03-03 18:08:02 +0200

    rofi rework

---
+++ pytfeeder/rofi/config.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from typing import Any, Dict
+
+from . import consts
+
+
+@dataclass
+class ConfigRofi:
+    alphabetic_sort: bool = False
+    channel_feed_limit: int = -1
+    channels_fmt: str = consts.DEFAULT_CHANNELS_FMT
+    datetime_fmt: str = consts.DEFAULT_DATETIME_FMT
+    entries_fmt: str = consts.DEFAULT_ENTRIES_FMT
+    feed_limit: int = -1
+    offset: int = consts.DEFAULT_OFFSET
+    separator: str = consts.DEFAULT_SEPARATOR
+
+    def parse_config_file(self, kw: Dict[str, Any]) -> None:
+        if alphabetic_sort := kw.get("alphabetic_sort"):
+            self.alphabetic_sort = bool(alphabetic_sort)
+        if channel_feed_limit := kw.get("channel_feed_limit"):
+            self.channel_feed_limit = channel_feed_limit
+        if channels_fmt := kw.get("channels_fmt"):
+            self.channels_fmt = channels_fmt
+        if datetime_fmt := kw.get("datetime_fmt"):
+            self.datetime_fmt = datetime_fmt
+        if entries_fmt := kw.get("entries_fmt"):
+            self.entries_fmt = entries_fmt
+        if feed_limit := kw.get("feed_limit"):
+            self.feed_limit = feed_limit
+        if separator := kw.get("separator"):
+            self.separator = separator
+
+    def parse_args(self, kw: Dict[str, Any]) -> None:
+        if channel_feed_limit := kw.get("channel_feed_limit"):
+            self.channel_feed_limit = channel_feed_limit
+        if channels_fmt := kw.get("channels_fmt"):
+            self.channels_fmt = channels_fmt
+        if datetime_fmt := kw.get("datetime_fmt"):
+            self.datetime_fmt = datetime_fmt
+        if entries_fmt := kw.get("entries_fmt"):
+            self.entries_fmt = entries_fmt
+        if feed_limit := kw.get("feed_limit"):
+            self.feed_limit = feed_limit
+        if separator := kw.get("separator"):
+            self.separator = separator
+
+    def __repr__(self) -> str:
+        repr_str = "rofi:\n"
+        repr_str += f"  alphabetic_sort: {self.alphabetic_sort}\n"
+        repr_str += f"  channel_feed_limit: {self.channel_feed_limit}\n"
+        repr_str += f"  channels_fmt: {self.channels_fmt}\n"
+        repr_str += f"  datetime_fmt: {self.datetime_fmt}\n"
+        repr_str += f"  entries_fmt: {self.entries_fmt}\n"
+        repr_str += f"  feed_limit: {self.feed_limit}\n"
+        repr_str += f"  offset: {self.offset}\n"
+        repr_str += f"  separator: {self.separator}\n"
+
+        return repr_str
--- pytfeeder/rofi/printer.py
+++ pytfeeder/rofi/printer.py
@@ -1,49 +1,40 @@
-import argparse
 from typing import List
 
-from pytfeeder.config import Config
 from pytfeeder.feeder import Feeder
 from pytfeeder.models import Entry
 
 
 class RofiPrinter:
-    def __init__(
-        self, feeder: Feeder, config: Config, args: argparse.Namespace
-    ) -> None:
-        self.config = config
+    def __init__(self, feeder: Feeder) -> None:
         self.feeder = feeder
-        self.channels_fmt = args.rofi_channels_fmt or self.config.rofi_channels_fmt
-        self.entries_fmt = args.rofi_entries_fmt or self.config.rofi_entries_fmt
-        self.limit = args.limit
-        self.offset = args.active_offset
-        self.separator = args.separator
-        if self.config.alphabetic_sort:
+        self.c = self.feeder.config.rofi
+        if self.c.alphabetic_sort:
             self.feeder.channels.sort(key=lambda c: c.title)
 
     def print_channels(self) -> None:
         self.print_message("%d unviewed entries" % self.feeder.unviewed_count())
-        print("\000data\037main", end=self.separator)
+        print("\000data\037main", end=self.c.separator)
         highlight = []
         unviewed_count = lambda _: 0
-        if "{unviewed_count}" in self.channels_fmt:
+        if "{unviewed_count}" in self.c.channels_fmt:
             unviewed_count = lambda channel_id: self.feeder.unviewed_count(channel_id)
         for i, channel in enumerate(self.feeder.channels):
             print(
-                self.channels_fmt.format(
+                self.c.channels_fmt.format(
                     id=channel.channel_id,
                     title=channel.title,
                     unviewed_count=unviewed_count(channel.channel_id),
                 ),
-                end=self.separator,
+                end=self.c.separator,
             )
             if channel.have_updates:
-                highlight.append(str(i + self.offset))
+                highlight.append(str(i + self.c.offset))
         if highlight:
-            print("\000active\037%s" % ",".join(highlight), end=self.separator)
+            print("\000active\037%s" % ",".join(highlight), end=self.c.separator)
 
     def print_feed(self) -> None:
-        print("\000data\037feed", end=self.separator)
-        entries = self.feeder.feed(self.limit or self.config.feed_limit)
+        print("\000data\037feed", end=self.c.separator)
+        entries = self.feeder.feed(self.c.feed_limit)
         if not entries:
             self.print_message("no entries")
             return
@@ -65,18 +56,16 @@ class RofiPrinter:
             message = f"{message}, {unviewed_count} unviewed entries"
 
         self.print_message(message)
-        print("\000data\037%s" % channel_id, end=self.separator)
+        print("\000data\037%s" % channel_id, end=self.c.separator)
 
-        if entries := self.feeder.channel_feed(
-            channel_id, self.limit or self.config.channel_feed_limit
-        ):
+        if entries := self.feeder.channel_feed(channel_id, self.c.channel_feed_limit):
             self.print_entries(entries)
 
     def print_entries(self, entries: List[Entry]) -> None:
         highlight = []
         for i, entry in enumerate(entries):
             if not entry.is_viewed:
-                highlight.append(str(i + self.offset))
+                highlight.append(str(i + self.c.offset))
 
             channel_title = self.feeder.channel_title(entry.channel_id)
             meta = channel_title
@@ -84,18 +73,18 @@ class RofiPrinter:
                 meta += "%s%s" % (",".join(parts), "".join(parts))
 
             print(
-                self.entries_fmt.format(
+                self.c.entries_fmt.format(
                     title=entry.title.replace("&", "&amp;"),
                     id=entry.id,
                     channel_title=channel_title,
                     meta=meta,
-                    updated=entry.updated.strftime(self.config.datetime_fmt),
+                    updated=entry.updated.strftime(self.c.datetime_fmt),
                 ),
-                end=self.separator,
+                end=self.c.separator,
             )
 
         if highlight:
-            print("\000active\037%s" % ",".join(highlight), end=self.separator)
+            print("\000active\037%s" % ",".join(highlight), end=self.c.separator)
 
     def print_message(self, message: str) -> None:
-        print("\000message\037%s" % message, end=self.separator)
+        print("\000message\037%s" % message, end=self.c.separator)
