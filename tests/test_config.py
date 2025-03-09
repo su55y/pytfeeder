d6b60f96357893df72a4be278ecce703d0514609
su55y <cesarmilano88@yahoo.com>
2025-03-09 16:13:19 +0200

    test_config upd

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,20 +1,37 @@
-import logging
-from pathlib import Path
 import unittest
-from pytfeeder.config import Config
-import pytfeeder.consts as c
+
+
+from pytfeeder.rofi.config import ConfigRofi
 from pytfeeder.tui import ConfigTUI
 
 
 class TestConfig(unittest.TestCase):
-    def test_default_config(self):
-        dc = Config()
-        self.assertEqual(dc.alphabetic_sort, False)
-        self.assertEqual(dc.cache_dir, Path.home() / ".cache/pytfeeder")
-        self.assertEqual(dc.datetime_fmt, c.DEFAULT_DATETIME_FMT)
-        self.assertEqual(dc.log_fmt, c.DEFAULT_LOG_FMT)
-        self.assertEqual(dc.log_level, logging.NOTSET)
-        self.assertEqual(dc.rofi_channels_fmt, c.DEFAULT_ROFI_CHANNELS_FMT)
-        self.assertEqual(dc.rofi_entries_fmt, c.DEFAULT_ROFI_ENTRIES_FMT)
-        self.assertEqual(dc.unwatched_first, False)
-        self.assertEqual(dc.tui, ConfigTUI())
+    def test_updating_tui_config(self):
+        c = ConfigTUI()
+        d = {
+            "alphabetic_sort": True,
+            "channel_feed_limit": 20,
+            "channels_fmt": "new fmt",
+        }
+
+        for k, v in d.items():
+            self.assertNotEqual(getattr(c, k), v)
+
+        c.update(d)
+        for k, v in d.items():
+            self.assertEqual(getattr(c, k), v)
+
+    def test_updating_rofi_config(self):
+        c = ConfigRofi()
+        d = {
+            "alphabetic_sort": True,
+            "channel_feed_limit": 20,
+            "channels_fmt": "new fmt",
+        }
+
+        for k, v in d.items():
+            self.assertNotEqual(getattr(c, k), v)
+
+        c.update(d)
+        for k, v in d.items():
+            self.assertEqual(getattr(c, k), v)
