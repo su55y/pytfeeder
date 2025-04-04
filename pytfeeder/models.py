f0a1581810c8e9cd7cfb6fcb4e0c90f0acb5c996
su55y <su55y@protonmail.com>
2025-04-04 09:09:17 +0300

    pytfeeder: add channel validation

--- pytfeeder/models.py
+++ pytfeeder/models.py
@@ -19,12 +19,24 @@ class Entry:
         )
 
 
+class InvalidChannelError(ValueError):
+    pass
+
+
 @dataclass
 class Channel:
     title: str = ""
     channel_id: str = ""
     entries: List[Entry] = field(default_factory=list)
     have_updates: bool = False
+
+    def __post_init__(self) -> None:
+        if self.title == "":
+            raise InvalidChannelError(f"Invalid title {self.title!r}")
+        if len(self.channel_id) != 24 and self.channel_id != "feed":
+            raise InvalidChannelError(
+                f"Invalid channel_id {len(self.channel_id) = } ({self.channel_id!r}), should be 24)"
+            )
 
     def dump(self) -> Dict:
         return {"title": self.title, "channel_id": self.channel_id}
