c1567fd1440917751ad85223de5af92c650d5c51
su55y <su55y@protonmail.com>
2025-04-17 10:44:02 +0300

    tests: storage/is_deleted

---
+++ tests/storage/test_storage_is_deleted.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import unittest
+
+from pytfeeder.storage import Storage
+from .. import mocks, utils
+
+
+class IsDeletedTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        utils.setup_logging(filename=f"{Path(__file__).name}.log")
+        cls.db_file = Path("/tmp/test_storage.db")
+        if cls.db_file.exists():
+            cls.db_file.unlink()
+        cls.stor = Storage(cls.db_file)
+        cls.entry = mocks.sample_entries[0]
+        assert cls.stor.add_entries([cls.entry]) == 1, "Insert test entry failed"
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.db_file.exists():
+            cls.db_file.unlink()
+
+    def test_is_deleted(self):
+        self.assertEqual(self.stor.select_entries_count(), 1)
+        self.assertFalse(self.stor.select_entries()[0].is_deleted)
+        self.assertTrue(self.stor.mark_entry_as_deleted(id=self.entry.id))
+        self.assertEqual(len(self.stor.select_entries()), 0)
+        self.assertEqual(self.stor.select_entries_count(), 1)
