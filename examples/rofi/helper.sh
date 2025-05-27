23f3a1eecd6d32d2798ef85acacdb769f91b2245
su55y <su55y@protonmail.com>
2025-05-27 17:56:12 +0300

    examples/rofi: cleanup

--- examples/rofi/helper.sh
+++ examples/rofi/helper.sh
@@ -3,59 +3,58 @@
 # optional for append
 APPEND_SCRIPT="${XDG_DATA_HOME:-$HOME/.local/share}/rofi/playlist_ctl_py/append_video.sh"
 # optional for download
-DOWNLOAD_DIR="$HOME/Videos/YouTube"
+DOWNLOAD_OUTPUT="$HOME/Videos/YouTube/%(uploader)s/%(title)s.%(ext)s"
 
-clean_title() {
-    title="unknown"
-    [ -n "$1" ] && title="$(echo "$1" | sed 's/\r.*//')"
-    printf '%s' "$title"
-}
-
-download_vid() {
-    [ "${#1}" -eq 11 ] || return
-    title="$(clean_title "$2")"
-    notify-send -a "pytfeeder" "⬇️Start downloading '$title'..."
-    qid="$(tsp yt-dlp "https://youtu.be/$1" -o "$DOWNLOAD_DIR/%(uploader)s/%(title)s.%(ext)s")"
-    tsp -D "$qid" notify-send -a "pytfeeder" "✅Download done: '$title'"
-}
+notify() { [ -n "$1" ] && notify-send -a pytfeeder "$1"; }
 
 err_msg() {
     [ -n "$1" ] && printf '\000message\037error: %s\n\000urgent\0370\n \000nonselectable\037true\n' "$1"
     exit 1
 }
 
+clean_title() {
+    echo "$1" | sed -E 's/<[^>]+>[^<]*<\/[^>]*>//g' | sed 's/\r.*//;s/^[ \t]*//;s/[ \t]*$//'
+}
+
+download_vid() {
+    [ "${#1}" -eq 11 ] || return
+    title="$(clean_title "$2")"
+    notify "⬇️Start downloading '$title'..."
+    qid="$(tsp yt-dlp "https://youtu.be/$1" -o "$DOWNLOAD_OUTPUT")"
+    tsp -D "$qid" notify-send -a pytfeeder "✅Download done: '$title'"
+}
+
+play() {
+    title=$(clean_title "$2")
+    notify "Playing '$title'..."
+    setsid -f mpv "$1" >/dev/null 2>&1
+}
+
 start_menu() {
-    printf "\000markup-rows\037true\n"
     pytfeeder-rofi "$@" \
         --channels-fmt '{title}\r<i><b>{unwatched}</b> new entries</i>\000info\037{id}\037active\037{active}'
-    printf "\000new-selection\0370\n"
+    printf '\000new-selection\0370\n'
 }
 
 print_feed() {
-    printf "back\000info\037main\n"
-    printf "\000markup-rows\037true\n"
+    printf 'back\000info\037main\n'
     pytfeeder-rofi "$@" -f \
         --feed-entries-fmt '{title}\r<b><i>{channel_title}</i></b> {published}\000info\037{id}\037meta\037{meta}\037active\037{active}' \
         --datetime-fmt '<i>%d %B</i>'
 }
 
 print_channel_feed() {
-    [ -n "$1" ] || return 1
+    [ "${#1}" -eq 24 ] || err_msg "invalid channel_id '$1'"
     channel_id="$1"
     shift
-    printf "back\000info\037main\n"
+    printf 'back\000info\037main\n'
     pytfeeder-rofi "$@" -i="$channel_id" \
         --entries-fmt '{title}\r<b>{published}</b>\000info\037{id}\037meta\037{meta}\037active\037{active}' \
         --datetime-fmt '<i>%d %B</i>'
 }
 
-play() {
-    title="$(clean_title "$2")"
-    notify-send -a "pytfeeder" "Playing '$title'..."
-    setsid -f mpv "$1" >/dev/null 2>&1
-}
-
-printf "\000use-hot-keys\037true\n"
+printf '\000use-hot-keys\037true\n'
+printf '\000markup-rows\037true\n'
 
 case $ROFI_RETV in
 # channels list on start
@@ -65,18 +64,16 @@ case $ROFI_RETV in
     case "$ROFI_INFO" in
     feed)
         print_feed
-        printf "\000new-selection\0370\n"
+        printf '\000new-selection\0370\n'
         ;;
     main) start_menu ;;
     *)
-        if [ "$(printf '%s' "$ROFI_INFO" |
-            grep -oP "^[0-9a-zA-Z_\-]{24}$")" = "$ROFI_INFO" ]; then
+        if echo "$ROFI_INFO" | grep -sP '^[-_0-9a-zA-Z]{24}$' >/dev/null 2>&1; then
             print_channel_feed "$ROFI_INFO"
-            printf "\000new-selection\0370\n"
-        elif [ "$(printf '%s' "$ROFI_INFO" |
-            grep -oP "^[0-9a-zA-Z_\-]{11}$")" = "$ROFI_INFO" ]; then
+            printf '\000new-selection\0370\n'
+        elif echo "$ROFI_INFO" | grep -sP '^[-_0-9a-zA-Z]{11}$' >/dev/null 2>&1; then
             pytfeeder-rofi -w="$ROFI_INFO" >/dev/null 2>&1
-            play "https://youtu.be/$ROFI_INFO" "$@"
+            play "https://youtu.be/$ROFI_INFO" "$1"
         else
             err_msg "invalid id '$ROFI_INFO'"
         fi
@@ -88,48 +85,36 @@ case $ROFI_RETV in
     case $ROFI_DATA in
     feed) print_feed "-s" ;;
     main) start_menu "-s" ;;
-    *)
-        [ "${#ROFI_DATA}" -eq 24 ] || err_msg "invalid channel_id '$ROFI_DATA'"
-        print_channel_feed "$ROFI_DATA" "-s"
-        ;;
+    *) print_channel_feed "$ROFI_DATA" "-s" ;;
     esac
     ;;
-# kb-custom-3 (Ctrl-x) -- mark entry as viewed
-# kb-custom-6 (Ctrl-d) -- download selected entry
+# kb-custom-2 (Ctrl-x) -- mark entry as viewed
+# kb-custom-5 (Ctrl-d) -- download selected entry
 11 | 14)
     [ "${#ROFI_INFO}" -eq 11 ] || err_msg "invalid id '$ROFI_INFO'"
     case $ROFI_DATA in
     feed) print_feed "-w=$ROFI_INFO" ;;
-    *)
-        [ "${#ROFI_DATA}" -eq 24 ] || err_msg "invalid channel_id '$ROFI_DATA'"
-        print_channel_feed "$ROFI_DATA" "-w=$ROFI_INFO"
-        ;;
+    *) print_channel_feed "$ROFI_DATA" "-w=$ROFI_INFO" ;;
     esac
     [ "$ROFI_RETV" -eq 14 ] && download_vid "$ROFI_INFO" "$1" >/dev/null 2>&1
     ;;
-# kb-custom-4 (Ctrl-X) -- mark current feed entries as viewed
+# kb-custom-3 (Ctrl-X) -- mark current feed entries as viewed
 12)
     case $ROFI_DATA in
     feed) print_feed "-w" "all" ;;
     main) start_menu "-w" "all" ;;
-    *)
-        [ "${#ROFI_DATA}" -eq 24 ] || err_msg "invalid channel_id '$ROFI_DATA'"
-        print_channel_feed "$ROFI_DATA" "-w=$ROFI_DATA"
-        ;;
+    *) print_channel_feed "$ROFI_DATA" "-w=$ROFI_DATA" ;;
     esac
-    printf "\000new-selection\0370"
+    printf '\000new-selection\0370'
     ;;
-# kb-custom-5 (Ctrl-a) -- append selected to playlist
+# kb-custom-4 (Ctrl-a) -- append selected to playlist
 13)
     [ -f "$APPEND_SCRIPT" ] || err_msg "append script not found"
     [ "${#ROFI_INFO}" -eq 11 ] || err_msg "invalid id '$ROFI_INFO'"
     setsid -f "$APPEND_SCRIPT" "https://youtu.be/$ROFI_INFO" >/dev/null 2>&1
     case $ROFI_DATA in
     feed) print_feed "-w=$ROFI_INFO" ;;
-    *)
-        [ "${#ROFI_DATA}" -eq 24 ] || err_msg "invalid channel_id '$ROFI_DATA'"
-        print_channel_feed "$ROFI_DATA" "-w=$ROFI_INFO"
-        ;;
+    *) print_channel_feed "$ROFI_DATA" "-w=$ROFI_INFO" ;;
     esac
     ;;
 esac
