71e92de238e4aaa215afb57840c74c1ffd65c824
su55y <su55y@protonmail.com>
2025-09-06 19:18:21 +0300

    examples/i3block: upd lock reading

--- examples/i3block/pytfeeder.sh
+++ examples/i3block/pytfeeder.sh
@@ -57,7 +57,7 @@ case $BLOCK_BUTTON in
 1)
     NOW=$(date +%s)
     if [ -f "$UPDATE_LOCK_PATH" ]; then
-        LAST_UPDATE_TIMESTAMP=$(cat "$UPDATE_LOCK_PATH")
+        LAST_UPDATE_TIMESTAMP=$(grep -oP '\d\:\K\d{10}$' "$UPDATE_LOCK_PATH")
         [ $((NOW - LAST_UPDATE_TIMESTAMP)) -gt $UPDATE_INTERVAL_SEC ] &&
             update
     else
